700143c2dd96c1d71540da424efc2b7406ca3581
Jude <jude@Judes-MacBook-Pro.local>
2022-08-12 16:32:07 -0400

    premier fichier php

---
+++ formulaire.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Process data</title>
+</head>
+<body>
+    <?php
+    $fname = $_GET["fname"];
+    $lname = $_GET["lname"];
+    $email = $_GET["email"];
+    $age = $_GET["age"];
+    echo "Bonjour " . $fname ." ". $lname ."<br>"; 
+    echo "Votre courriel est : " . $email."<br>";
+    echo "Vous avez : " . $age. " ans " ."<br>"; 
+    ?>
+</body>
+</html>
+
+
+
+
+
